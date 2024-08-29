9acde8f98c7e14762ef2ca74330d20380e39c3ca
mehranhydary <mehranhydary@users.noreply.github.com>
2024-08-29 03:20:37 -0400

    Fixed some of the remappings

--- src/MiladyPoolTaskManager.sol
+++ src/MiladyPoolTaskManager.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: VPL-1.0
 pragma solidity ^0.8.26;
 
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
 import "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
 import "@eigenlayer/contracts/permissions/Pausable.sol";
@@ -13,6 +19,7 @@ import "@eigenlayer-middleware/src/libraries/BN254.sol";
 import "./interfaces/IMiladyPoolTaskManager.sol";
 
 contract MiladyPoolTaskManager is
+    BaseHook,
     OwnableUpgradeable,
     Pausable,
     BLSSignatureChecker,
@@ -20,6 +27,8 @@ contract MiladyPoolTaskManager is
     IMiladyPoolTaskManager
 {
     using BN254 for BN254.G1Point;
+    using StateLibrary for IPoolManager;
+    using PoolIdLibrary for PoolKey;
 
     // TODO: Do we need order response / challenge times?
     // uint32 public immutable TASK_RESPONSE_WINDOW_BLOCK;
@@ -29,9 +38,35 @@ contract MiladyPoolTaskManager is
     // TODO: Figure out if we need latest order id, order hashes, zkps, matching order ids, responses, challenges, etc.
 
     constructor(
-        IRegistryCoordinator _registryCoordinator
-    ) BLSSignatureChecker(_registryCoordinator) {
+        IRegistryCoordinator _registryCoordinator,
+        IPoolManager _poolManager
+    ) BLSSignatureChecker(_registryCoordinator) BaseHook(_poolManager) {
         TASK_RESPONSE_WINDOW_BLOCK = 100;
+    }
+
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: true,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
     }
 
     function initialize(
