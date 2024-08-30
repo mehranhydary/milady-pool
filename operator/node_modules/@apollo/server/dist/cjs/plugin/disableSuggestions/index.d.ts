3e61104ea5aab04508c4c7883b206bd71c56e4f0
mehranhydary <mehranhydary@users.noreply.github.com>
2024-08-30 00:59:36 -0400

    Removed node moudles and dist

--- operator/node_modules/@apollo/server/dist/cjs/plugin/disableSuggestions/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ApolloServerPlugin } from '../../externalTypes/index.js';
-export declare function ApolloServerPluginDisableSuggestions(): ApolloServerPlugin;
-//# sourceMappingURL=index.d.ts.map
--- operator/node_modules/@apollo/server/dist/cjs/utils/resolvable.d.ts
+++
@@ -1,7 +0,0 @@
-export type Resolvable<T> = Promise<T> & {
-    resolve: (t: T) => void;
-    reject: (e: any) => void;
-};
-declare const _default: <T = void>() => Resolvable<T>;
-export default _default;
-//# sourceMappingURL=resolvable.d.ts.map
