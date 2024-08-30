6fe99c3e9fbb8755eaaeca15811ca433b2716f58
mehranhydary <mehranhydary@users.noreply.github.com>
2024-08-30 01:05:40 -0400

    latest

--- operator/.build/dist/config/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serverConfig = void 0;
-exports.serverConfig = {
-    port: 8081,
-};
--- operator/.build/dist/core/graphql/extensions/orders/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = exports.typeDefs = void 0;
-const graphql_tag_1 = __importDefault(require("graphql-tag"));
-exports.typeDefs = (0, graphql_tag_1.default) ``;
-exports.resolvers = {};
