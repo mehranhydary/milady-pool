633807dd4c20bc8066c31edf3efcc10f6799ac52
mehranhydary <mehranhydary@users.noreply.github.com>
2024-09-02 15:00:50 -0400

    Writing a simple rust program to generate proof for fib numbers... need to replace for our orders eventually

---
+++ program/src/main.rs
@@ -0,0 +1,29 @@
+// TODO: Add comments
+
+#![no_main]
+sp1_zkvm:entrypoint!(main);
+
+use alloy_sol_types::SolType;
+use milady_pool_lib::{
+  // What does your lib do?
+  fibonacci,
+  PublicValuesStruct
+};
+
+pub fn main() {
+    // Read an input to the program.
+    //
+    // Behind the scenes, this compiles down to a custom system call which handles reading inputs
+    // from the prover.
+    let n = sp1_zkvm::io::read::<u32>();
+
+    // Compute the n'th fibonacci number using a function from the workspace lib crate.
+    let (a, b) = fibonacci(n);
+
+    // Encode the public values of the program.
+    let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct { n, a, b });
+
+    // Commit to the public values of the program. The final proof will have a commitment to all the
+    // bytes that were committed to.
+    sp1_zkvm::io::commit_slice(&bytes);
+}
