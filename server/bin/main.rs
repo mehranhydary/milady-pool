e80a2f106a8bfa0e96e5fe57689d604e8ecf392f
mehranhydary <mehranhydary@users.noreply.github.com>
2024-09-05 20:03:57 -0400

    Working on REST api

--- server/bin/main.rs
+++ server/bin/main.rs
@@ -1,28 +1,51 @@
-use std::net::TcpListener;
+// use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs;
 use std::thread;
 use std::time::Duration;
-use server::ThreadPool;
+use milady_pool_server::ThreadPool;
+use milady_pool_lib::fibonacci;
 
+// New
+use axum::{
+  extract::{Path, State},
+  http::StatusCode,
+  routing::{get, post},
+  Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use tokio::net::TcpListener;
 
+#[tokio::main]
+async fn main() {
+  let server_address = "127.0.0.1:7878";
+  let listener = TcpListener::bind(server_address).await.expect("Could not create TCP Listener");
 
-fn main () {
-  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  println!("Server listening on port 7878");
 
-  let pool = ThreadPool::new(4);
+  let app = Router::new()
+    .route("/", get(|| async { "Welcome to Milady Pool ZKP API!" }));
+    // .route("/fibonacci/:id", get(fibonacci_handler))
+    // .route("/log", post(log_handler));
 
-  for stream in listener.incoming() {
-    let stream  = stream.unwrap();
-
-    pool.execute(|| {
-      handle_connection(stream);
-    });
-  }
-
+  axum::serve(listener,app).await.expect("Server failed to start");
 
 }
+// fn main () {
+//   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+//   let pool = ThreadPool::new(4);
+
+//   for stream in listener.incoming() {
+//     let stream  = stream.unwrap();
+
+//     pool.execute(|| {
+//       handle_connection(stream);
+//     });
+//   }
+// }
 
 fn handle_connection(mut stream: TcpStream) {
   let mut buffer = [0; 1024];
@@ -30,12 +53,25 @@ fn handle_connection(mut stream: TcpStream) {
 
   let get = b"GET / HTTP/1.1\r\n";
   let sleep = b"GET /sleep HTTP/1.1\r\n";
+  let post = b"POST /log HTTP/1.1\r\n";
 
-  if buffer.starts_with(get) {
+
+  if buffer.starts_with(post) {
+    let contents = String::from_utf8_lossy(&buffer[..]);
+    println!("Received POST request with data: {}", contents);
+    // TODO: Come back to this
+
+  } else if buffer.starts_with(get) {
     send_response(stream, "HTTP/1.1 200 OK", "server/index.html");
   } else if buffer.starts_with(sleep) {
     thread::sleep(Duration::from_secs(5));
     send_response(stream, "HTTP/1.1 200 OK", "server/index.html");
+  } else if buffer.starts_with(get) {
+    let id = 5;
+    let (a, b) = call_fibonacci(id);
+    let response = format!("HTTP/1.1 200 OK\r\n\r\nThe fibonacci number at position {} is: {} and {}", id, a, b);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
   } else {
     send_response(stream, "HTTP/1.1 404 NOT FOUND", "server/404.html");
   }
@@ -52,3 +88,11 @@ fn send_response(mut stream: TcpStream, status_line: &str, filename: &str) {
   stream.write(response.as_bytes()).unwrap();
   stream.flush().unwrap();
 }
+
+fn call_fibonacci(n: u32) -> (u32, u32) {
+  let (a, b)= fibonacci(n);
+  (a, b)
+}
+
+
+
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,10 +1,15 @@
 use std::{sync::{mpsc, Arc, Mutex}, thread};
 pub struct ThreadPool {
   workers: Vec<Worker>,
-  sender: mpsc::Sender<Job>,
+  sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+  NewJob(Job),
+  Terminate,
+}
 
 impl ThreadPool {
   
@@ -30,25 +35,54 @@ impl ThreadPool {
     F: FnOnce() + Send + 'static
   {
     let job = Box::new(f); 
-    self.sender.send(job).unwrap();
+    self.sender.send(Message::NewJob(job)).unwrap();
+  }
+}
+
+impl Drop for ThreadPool {
+  fn drop(&mut self) {
+    println!("Sending terminate message to all workers.");
+    for _ in &self.workers {
+      self.sender.send(Message::Terminate).unwrap();
+    }
+
+    println!("Shutting down all workers.");
+
+    for worker in &mut self.workers {
+      println!("Shutting down worker {}", worker.id);
+
+      if let Some(thread) = worker.thread.take() {
+        thread.join().unwrap()
+      }
+    }
   }
 }
 
 struct Worker {
   id: usize,
-  thread: thread::JoinHandle<()>,
+  thread: Option<thread::JoinHandle<()>>
 }
 
 impl Worker {
-  fn new(id:usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+  fn new(id:usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
     let thread = thread::spawn(move || loop {
-      let job = receiver.lock().unwrap().recv().unwrap();
+      let message = receiver.lock().unwrap().recv().unwrap();
       
-      println!("Worker {} got a job; executing.", id);
+      match message {
+        Message::NewJob(job) => {
+          println!("Worker {} got a job; executing.", id);
+          job();
+        }
+        Message::Terminate => {
+          println!("Worker {} was told to terminate.", id);
+          break;
+        }
+      }
 
-      job();
     });
 
-    Worker { id, thread }
+
+
+    Worker { id, thread: Some(thread) }
   }
 }
